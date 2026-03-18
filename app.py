65c679d001b0e37b79992784d77ee86cd27b122a
C.Y. Chan <2460157@dundee.ac.uk>
2026-03-18 12:33:11 +0000

    test python

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////workspace/database.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the F1 Team Management System!"
+
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+        print(f"Database created at {db_path}")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
